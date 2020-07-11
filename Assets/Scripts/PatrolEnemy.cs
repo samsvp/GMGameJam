77b06dda0832b349aa47f15acb44abb5adb35266
vitor pavani <vitorpavani@poli.ufrj.br>
2020-07-11 02:08:55 -0300

    add enemy script

---
+++ Assets/Scripts/PatrolEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PatrolEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb2D;
+    private Transform transform;
+    public float patrolRange;
+    public float speed;
+    private Vector3 initialPosition;
+    public bool facingLeft = true;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        transform = GetComponent<Transform>();
+        initialPosition = transform.position;
+        Debug.Log(initialPosition.x);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((initialPosition.x - patrolRange) > transform.position.x)
+        {
+            facingLeft = true;
+        }
+        if ((initialPosition.x + patrolRange) < transform.position.x)
+        {
+            facingLeft = false;
+        }
+        if (facingLeft)
+        {
+            transform.Translate(1 * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-1 * Time.deltaTime, 0, 0);
+        }
+    }
+}
